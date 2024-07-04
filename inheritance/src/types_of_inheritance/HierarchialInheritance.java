68e99bac3a7089de6dd3467844e8489c7c8b3bbd
Dell <Dell@KUSHI-LAPTOP>
2024-07-04 19:07:44 +0530

    inheritance

---
+++ inheritance/src/types_of_inheritance/HierarchialInheritance.java
@@ -0,0 +1,19 @@
+package types_of_inheritance;
+class Animaaal{
+	
+}
+class Tiger extends Animaaal{
+	
+}
+class monkey extends Animaaal{
+	
+}
+class Deer extends Animaaal{
+	
+	
+}
+public class HierarchialInheritance {
+public static void main(String args[]) {
+		
+	}
+}
