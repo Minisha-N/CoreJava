c3289265cdc375f12b0f5fa51da14438fe45ebcb
Minisha-N <minisha9353@gmail.com>
2024-07-07 15:33:57 +0530

    poly

--- Polymorphism/src/polymorphism/MethodOverloading.java
+++ Polymorphism/src/polymorphism/MethodOverloading.java
@@ -1,5 +1,5 @@
 package polymorphism;
-class Adder{
+class Addder{
 	int add(int a ,int b) {
 		return a+b;
 	}
@@ -14,7 +14,7 @@ public class MethodOverloading {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Adder a1=new Adder();
+		Addder a1=new Addder();
 		System.out.println(a1.add(3,4));
 		System.out.println(a1.add(3,5,4));
 		System.out.println(a1.add(3.3f,5.5f));
